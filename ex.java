6b10e2a76f2b64cdd6d2c121b23ad2e1b08e8183
adityamshetty <adityamshetty2000@gmail.com>
2021-04-18 22:14:23 +0530

    initial commit

---
+++ ex.java
@@ -0,0 +1,14 @@
+public class ex
+{
+	//anc
+	public static void main (String[] args) 
+	{ 
+		int a=1;
+		int b=12*4/2+1;	
+		for(int i=0;i<5;i=i+1)
+		{
+			int a=5+3*3/2;	
+		}
+		//int d=5;
+	}
+}
